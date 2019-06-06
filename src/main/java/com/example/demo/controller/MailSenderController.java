6decbf0bf81a1b177fe404b8aa25cfd5c6cb089b
Fedia23 <fedia.ch@gmail.com>
2019-06-06 13:00:13 +0300

    homework : jdbc connection to postgresql

--- src/main/java/com/example/demo/controller/MailSenderController.java
+++ src/main/java/com/example/demo/controller/MailSenderController.java
@@ -6,7 +6,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
@@ -18,7 +25,7 @@ public class MailSenderController {
     @Autowired
     private MessageRepository messageRepository;
 
-    @GetMapping
+    @RequestMapping
     public List<Message> getMessages() {
         List<Message> sender = messageRepository.getAllMessage();
         return sender;
