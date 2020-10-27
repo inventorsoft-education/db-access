f811adafe882fd4e1ae4d3c08bb336d18fb6f507
Alex <paskar.olexandr@gmail.com>
2020-10-27 17:26:45 +0200

    proxyBeanMethods = false

--- src/main/java/com/paskar/email/application/EmailDeliveryApplication.java
+++ src/main/java/com/paskar/email/application/EmailDeliveryApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 @EnableScheduling
 public class EmailDeliveryApplication {
     public static void main(String[] args) {
--- src/main/java/com/paskar/email/application/configuration/SpringConfig.java
+++ src/main/java/com/paskar/email/application/configuration/SpringConfig.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class SpringConfig {
     @Bean
     public ObjectMapper mapper() {
