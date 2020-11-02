1ca892206d7e14060121f7750300640e01986547
Alex <paskar.olexandr@gmail.com>
2020-11-02 18:09:11 +0200

    DataSource config has been moved to application.properties

--- src/main/java/com/paskar/email/application/configuration/HibernateConfig.java
+++ src/main/java/com/paskar/email/application/configuration/HibernateConfig.java
@@ -2,18 +2,15 @@ package com.paskar.email.application.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
 public class HibernateConfig {
-
 
     @Bean
     public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
@@ -21,15 +18,4 @@ public class HibernateConfig {
         tm.setEntityManagerFactory(entityManagerFactory);
         return tm;
     }
-
-    @Bean
-    public DataSource getDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/academy_hibernate_homework");
-        dataSource.setUsername("mysql");
-        dataSource.setPassword("mysql");
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        return dataSource;
-    }
-
 }
