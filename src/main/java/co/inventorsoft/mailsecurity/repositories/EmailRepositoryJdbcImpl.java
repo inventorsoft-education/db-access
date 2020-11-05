617d9afcf679e426c1a5d165c4775c263869f90a
Oleh Kilaru <kilaruoleh@gmail.com>
2020-11-05 13:25:58 +0200

    change JdbcTemplate to Statement and PreparedStatement

--- src/main/java/co/inventorsoft/mailsecurity/repositories/EmailRepositoryJdbcImpl.java
+++ src/main/java/co/inventorsoft/mailsecurity/repositories/EmailRepositoryJdbcImpl.java
@@ -1,73 +1,120 @@
 package co.inventorsoft.mailsecurity.repositories;
 
 import co.inventorsoft.mailsecurity.models.Email;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Repository
 public class EmailRepositoryJdbcImpl implements EmailRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+    private Connection connection;
 
-    public EmailRepositoryJdbcImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    private Map<Integer, Email> emailMap = new HashMap<>();
-
-
-    private RowMapper<Email> emailRowMapper = (ResultSet resultSet, int i) -> {
-        Integer id = resultSet.getInt("id");
-
-        if (!emailMap.containsKey(id)) {
-            String recipient = resultSet.getString("recipient");
-            String subject = resultSet.getString("subject");
-            String body = resultSet.getString("body");
-            LocalDateTime date = LocalDateTime.parse(resultSet.getString("date"), DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
-            Email email = new Email(id, recipient, subject, body, date);
-            emailMap.put(id, email);
+    public EmailRepositoryJdbcImpl(DataSource dataSource) {
+        try {
+            this.connection = dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
         }
-        return emailMap.get(id);
-    };
+    }
 
     @Override
     public List<Email> findAll() {
         String SQL_SELECT_ALL = "SELECT * FROM emails";
-        return jdbcTemplate.query(SQL_SELECT_ALL, emailRowMapper);
+        try {
+            List<Email> emails = new ArrayList<>();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SQL_SELECT_ALL);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String recipient = resultSet.getString("recipient");
+                String subject = resultSet.getString("subject");
+                String body = resultSet.getString("body");
+                LocalDateTime date = LocalDateTime.parse(resultSet.getString("date"), DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+
+                Email email = new Email(id, recipient, subject, body, date);
+                emails.add(email);
+            }
+            return emails;
+        }catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     @Override
     public void saveMail(Email email) {
         String SQL_INSERT = "INSERT INTO emails(recipient,subject, body, date) VALUES (?,?,?,?)";
-        jdbcTemplate.update(SQL_INSERT, email.getRecipient(), email.getSubject(), email.getBody(), email.getDate());
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_INSERT);
+            preparedStatement.setString(1, email.getRecipient());
+            preparedStatement.setString(2, email.getSubject());
+            preparedStatement.setString(3, email.getBody());
+            preparedStatement.setString(4, email.getDate().toString());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     @Override
     public void update(Email email) {
         String SQL_UPDATE = "UPDATE emails SET recipient=?, subject=?, body=?, date=? WHERE id=?";
-        jdbcTemplate.update(SQL_UPDATE, email.getRecipient(), email.getSubject(), email.getBody(), email.getDate(), email.getId());
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_UPDATE);
+            preparedStatement.setString(1, email.getRecipient());
+            preparedStatement.setString(2, email.getSubject());
+            preparedStatement.setString(3, email.getBody());
+            preparedStatement.setString(4, email.getDate().toString());
+            preparedStatement.setInt(5, email.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     @Override
     public Email findById(int id) {
         String SQL_FIND_BY_ID = "SELECT * FROM emails WHERE id=?";
-        return jdbcTemplate.queryForObject(SQL_FIND_BY_ID, emailRowMapper, id);
+        Email email = null;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_FIND_BY_ID);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String recipient = resultSet.getString("recipient");
+                String subject = resultSet.getString("subject");
+                String body = resultSet.getString("body");
+                LocalDateTime date = LocalDateTime.parse(resultSet.getString("date"), DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+
+                email = new Email(id, recipient, subject, body, date);
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+        return email;
     }
 
     @Override
     public void delete(Email email) {
         String SQL_DELETE = "DELETE FROM emails WHERE id=?";
-        jdbcTemplate.update(SQL_DELETE, email.getId());
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_DELETE);
+            preparedStatement.setInt(1, email.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     @Override
