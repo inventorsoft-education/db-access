9019af5cdb701a755c5a05583539bd35076d224e
Oleh Khalus <oleh0704@gmail.com>
2020-11-16 19:12:12 +0200

    update homework

---
+++ src/main/java/co/inventorsoft/jdbc/configuration/HibernateConfig.java
@@ -0,0 +1,20 @@
+package co.inventorsoft.jdbc.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfig {
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        var transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+}
