4493abb3ac2c35918620f2c39d53fea19bc52846
Stas Kachurovskiy's <larevoo@gmail.com>
2021-07-15 14:28:23 +0300

    SQL queries were reformatted

--- src/main/java/co/inventrosoft/hibernate_task/repository/MatchRepository.java
+++ src/main/java/co/inventrosoft/hibernate_task/repository/MatchRepository.java
@@ -11,11 +11,21 @@ import java.util.Optional;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Integer> {
-    @Query("select m from Match m where m.tournament.id = ?1 order by m.roundCode desc, m.order")
+    @Query("""
+        select m from Match m 
+        where m.tournament.id = ?1 
+        order by m.roundCode desc, 
+                 m.order
+     """)
     List<Match> findAllByTournamentId(int tournamentId);
 
     Optional<Match> findByRoundCodeAndOrderAndTournamentId(int roundCode, int order, int tournamentId);
 
-    @Query("select m from Match m where m.firstTeam.name = ?1 and m.secondTeam.name = ?2 and m.tournament.id = ?3")
+    @Query("""
+        select m from Match m 
+        where m.firstTeam.name = ?1 and 
+              m.secondTeam.name = ?2 and 
+              m.tournament.id = ?3
+    """)
     Match getByTeamNamesAndTournamentId(String firstTeamName, String secondTeamName, int tournamentId);
 }
