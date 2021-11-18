93080767f93d2b58cf9db94c5c7649180df5679f
tsiupa-coder <ivan.tsiupa111@gmail.com>
2021-11-18 23:26:48 +0200

    deleted unused files

--- hibernate/src/main/java/com/inventorsoft/author/GameController.java
+++ hibernate/src/main/java/com/inventorsoft/author/GameController.java
@@ -6,35 +6,26 @@ import com.inventorsoft.domain.model.Tournament;
 import com.inventorsoft.domain.service.GameService;
 import com.inventorsoft.domain.service.TeamService;
 import com.inventorsoft.domain.service.TournamentService;
+
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Controller {
 
     TeamService teamService;
     GameService gameService;
     TournamentService tournamentService;
-
-    @Autowired
-    public Controller(TeamService teamService, GameService gameService, TournamentService tournamentService) {
-        this.teamService = teamService;
-        this.gameService = gameService;
-        this.tournamentService = tournamentService;
-        Team team = teamService.createTestTeam();
-        Team team1 = teamService.createTestTeam();
-        Tournament tournament = tournamentService.createTestTournament();
-        Game game = gameService.createTestGame(team, team1, tournament);
-    }
 
     @GetMapping(value = "/games")
     public ResponseEntity<List<Game>> games() {
@@ -42,7 +33,7 @@ public class Controller {
     }
 
     @GetMapping(value = "/games/{id}")
-    public ResponseEntity<Game> games(@PathVariable Integer id) {
+    public ResponseEntity<Game> getGamesById(@PathVariable Integer id) {
         return ResponseEntity.ok(gameService.getById(id));
     }
 
