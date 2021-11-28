0e84e963828cba57d2999c8016127ab052e486d7
Max Semchenko <sem4enko_max@ukr.net>
2021-11-28 21:35:09 +0200

    jdbc task

--- src/main/java/com/example/demo/service/SQLMethods.java
+++ src/main/java/com/example/demo/service/SQLMethods.java
@@ -3,7 +3,6 @@ package com.example.demo.service;
 import com.example.demo.entity.Team;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.sql.*;
 
 @Service
