c17f7650db544697122ba37ec3775e238cf31f00
Vlad Zhloba <zhloba96@gmail.com>
2021-12-01 22:09:29 +0200

    actualize run feature

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 import com.example.demo.console.ConsoleInOut;
-import com.example.demo.dao.TeamDAO;
 import com.example.demo.model.Team;
-import lombok.AllArgsConstructor;
+import com.example.demo.tournament.Tournament;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,10 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 
 @SpringBootApplication
-@AllArgsConstructor
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class DemoApplication implements CommandLineRunner {
-
-    private final ConsoleInOut console;
+    Tournament tournament;
+    ConsoleInOut console;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -22,19 +25,19 @@ public class DemoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        TeamDAO.save(List.of(
-                new Team("name1","capitan1","coach1"),
-                new Team("name2","capitan2","coach2"),
-                new Team("name3","capitan3","coach3"),
-                new Team("name4","capitan4","coach4"),
-                new Team("name5","capitan5","coach5"),
-                new Team("name6","capitan6","coach6"),
-                new Team("name7","capitan7","coach7"),
-                new Team("name8","capitan8","coach8")
+        tournament.saveAllTeams(List.of(
+                new Team("name1","captain1","coach1"),
+                new Team("name2","captain2","coach2"),
+                new Team("name3","captain3","coach3"),
+                new Team("name4","captain4","coach4"),
+                new Team("name5","captain5","coach5"),
+                new Team("name6","captain6","coach6"),
+                new Team("name7","captain7","coach7"),
+                new Team("name8","captain8","coach8")
                 ));
 
-        console.getTournament().createAndSaveMatchesBasedOnListOfTeams(TeamDAO.getList());
-        console.outMatchList();
-        console.outWinner();
+        tournament.createAndSaveMatchesBasedOnListOfTeams(tournament.getAllTeams());
+        console.printMatchList(tournament.getAllMatches());
+        console.printWinner();
     }
 }
