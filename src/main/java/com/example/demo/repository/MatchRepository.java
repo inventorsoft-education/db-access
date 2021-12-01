62eaf9c8d824d780cbc8f7a05e13e3da8547ad30
Vlad Zhloba <zhloba96@gmail.com>
2021-12-01 18:09:08 +0200

    added feauture with query for get list matches with teams

--- src/main/java/com/example/demo/repository/MatchRepository.java
+++ src/main/java/com/example/demo/repository/MatchRepository.java
@@ -2,8 +2,16 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Integer> {
+    @Query(name = "matches.getAllWithTeams",
+            value = "SELECT m from Match m " +
+                    "left join fetch m.firstTeam " +
+                    "left join fetch m.secondTeam")
+    List<Match> getFullList();
 }
