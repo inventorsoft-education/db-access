2f8f9431caeea6e42fe5e8b64a030059f3042756
Vlad Zhloba <zhloba96@gmail.com>
2021-12-01 22:08:44 +0200

    fixed annotations, fixed constructor, added constructor

--- src/main/java/com/example/demo/model/Match.java
+++ src/main/java/com/example/demo/model/Match.java
@@ -1,32 +1,43 @@
 package com.example.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 import java.util.Random;
 
-@AllArgsConstructor
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Match {
-    private String firstTeam;
-    private String secondTeam;
-    private String round;
-    private String score;
+    String firstTeam;
+    String secondTeam;
+    String round;
+    String score;
 
-    public Match(String firstTeam, String secondTeam, int round, int cntRounds, String score) {
+    public Match(String firstTeam, String secondTeam, int round, int cntRounds) {
         this.firstTeam = firstTeam;
         this.secondTeam = secondTeam;
         setRound(round, cntRounds);
+        this.score = generateScore();
+    }
+
+    public Match(String firstTeam, String secondTeam, String round, String score) {
+        this.firstTeam = firstTeam;
+        this.secondTeam = secondTeam;
+        this.round = round;
         this.score = score;
     }
 
     public void setRound(int round, int cntRounds) {
-        if ((round > cntRounds) || (round < 0))
+        if ((round > cntRounds) || (round < 0)) {
             throw new IllegalArgumentException("round must be > 0 and <=" + cntRounds);
-        else this.round = String.format("%d/%d", round, cntRounds);
+        } else {
+            this.round = String.format("%d/%d", round, cntRounds);
+        }
     }
 
-    public static String generateScore() {
+    private String generateScore() {
         Random random = new Random();
         Integer first = random.nextInt(6), second = random.nextInt(6);
         while (first.equals(second)) {
@@ -37,15 +48,17 @@ public class Match {
     }
 
     public void setScore(int scoreFirstTeam, int scoreSecondTeam) {
-        if ((scoreFirstTeam < 0 || scoreFirstTeam > 5) || (scoreSecondTeam < 0 || scoreSecondTeam > 5))
+        if ((scoreFirstTeam < 0 || scoreFirstTeam > 5) || (scoreSecondTeam < 0 || scoreSecondTeam > 5)) {
             throw new IllegalArgumentException("score must be >= 0 and <= 5");
-        else this.score = Integer.toString(scoreFirstTeam + ':' + scoreSecondTeam);
+        } else {
+            this.score = Integer.toString(scoreFirstTeam + ':' + scoreSecondTeam);
+        }
     }
 
     public String getWinnerName() {
         int scoreFirstTeam = Integer.parseInt(score.substring(0, score.lastIndexOf(":")));
         int scoreSecondTeam = Integer.parseInt(score.substring(score.lastIndexOf(":") + 1));
 
-        return (scoreFirstTeam > scoreSecondTeam) ? firstTeam : secondTeam;
+        return scoreFirstTeam > scoreSecondTeam ? firstTeam : secondTeam;
     }
 }
