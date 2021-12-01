80893da3cef6103aae43128fdc6800316baa067f
Vlad Zhloba <zhloba96@gmail.com>
2021-12-01 22:01:44 +0200

    Used DI, annotation fixed, made a Set instead List

--- src/main/java/com/example/demo/tournament/Tournament.java
+++ src/main/java/com/example/demo/tournament/Tournament.java
@@ -4,77 +4,71 @@ import com.example.demo.model.Match;
 import com.example.demo.model.Team;
 import com.example.demo.dao.MatchDAO;
 import com.example.demo.dao.TeamDAO;
-import lombok.AllArgsConstructor;
-import lombok.Value;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Component;
 
-import java.rmi.UnexpectedException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @Component
-@Value
-@AllArgsConstructor
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class Tournament {
+    MatchDAO matchDAO;
+    TeamDAO teamDAO;
 
-    public boolean createAndSaveMatchesBasedOnListOfTeams(List<Team> teams) {
+    public void createAndSaveMatchesBasedOnListOfTeams(List<Team> teams) {
         List<Match> matches = new ArrayList<>();
 
         if (teams.size() % 2 == 0) {
-            Team firstTmpTeam;
-            Team secondTmpTeam;
-            int firstRandomValue;
-            int secondRandomValue;
+            Team firstTmpTeam, secondTmpTeam;
+            Integer firstRandomValue, secondRandomValue, currentRound;
             int countMatchesAtTheRound = teams.size() / 2;
             int cntOfMatches = teams.size() - 1;
-            int currentRound;
-            int cntOfRounds;
-            List<Integer> indexes = generateIndexList(teams.size());
-            List<Integer> tmpIndexes;
-            cntOfRounds = returnCntOfRounds(cntOfMatches);
+            int cntOfRounds = returnCntOfRounds(cntOfMatches);
+            Set<Integer> indexes = generateIndexSet(teams.size());
+            Set<Integer> tmpIndexes;
 
             for (int i = 1; i <= cntOfRounds; i++) {
                 currentRound = i;
-                tmpIndexes = new ArrayList<>();
+                tmpIndexes = new HashSet<>();
                 for (int j = 0; j < countMatchesAtTheRound; j++) {
-                    firstRandomValue = getRandomValueFromArray(indexes);
-                    indexes.remove((Object) firstRandomValue);
-                    secondRandomValue = getRandomValueFromArray(indexes);
-                    indexes.remove((Object) secondRandomValue);
+                    firstRandomValue = getValueFromSet(indexes);
+                    indexes.remove(firstRandomValue);
+                    secondRandomValue = getValueFromSet(indexes);
+                    indexes.remove(secondRandomValue);
 
                     firstTmpTeam = teams.get(firstRandomValue);
                     secondTmpTeam = teams.get(secondRandomValue);
 
-                    Match match = new Match(firstTmpTeam.getName(), secondTmpTeam.getName(), currentRound, cntOfRounds, Match.generateScore());
+                    Match match = new Match(firstTmpTeam.getName(), secondTmpTeam.getName(), currentRound, cntOfRounds);
                     matches.add(match);
 
-                    if (getWinner(match).getName().equals(firstTmpTeam.getName())) {
+                    if ((getWinner(match).getName().equals(firstTmpTeam.getName()))) {
                         tmpIndexes.add(firstRandomValue);
+                    } else {
+                        tmpIndexes.add(secondRandomValue);
                     }
-                    else tmpIndexes.add(secondRandomValue);
                 }
-                indexes = new ArrayList<>(tmpIndexes);
+                indexes = new HashSet<>(tmpIndexes);
                 countMatchesAtTheRound /= 2;
             }
         }
-        MatchDAO.save(matches);
-        return true;
+        matchDAO.save(matches);
     }
 
-    private Integer getRandomValueFromArray(List<Integer> indexes) {
-        if(indexes.size() == 1) return indexes.get(indexes.size()-1);
-
-        Random random = new Random();
-        Integer tmp = random.nextInt(indexes.get(indexes.size()-1));
-        while (!indexes.contains(tmp)) {
-            tmp = random.nextInt(indexes.get(indexes.size()-1));
+    private Integer getValueFromSet(Set<Integer> indexes) {
+        Integer tempIndex = null;
+        for(Integer index: indexes) {
+            tempIndex = index;
+            break;
         }
-        return tmp;
+        return tempIndex;
     }
 
     private Integer returnCntOfRounds(int cntOfMatches) {
-        Integer tmp = cntOfMatches + 1;
+        int tmp = cntOfMatches + 1;
         Integer i = 0;
         while (tmp > 1) {
             tmp = tmp / 2;
@@ -83,22 +77,33 @@ public class Tournament {
         return i;
     }
 
-    private List<Integer> generateIndexList(int indexListSize) {
-        List<Integer> arr = new ArrayList<>();
+    private Set<Integer> generateIndexSet(int indexListSize) {
+        Set<Integer> setOfIndexes = new HashSet<>();
         for (int i = 0; i < indexListSize; i++) {
-            arr.add(i);
+            setOfIndexes.add(i);
         }
-        return arr;
+        return setOfIndexes;
     }
 
     public Team getWinner() {
-        List<Match> matches = MatchDAO.getList();
-        Team winner = TeamDAO.getTeamByName(matches.get(matches.size() - 1).getWinnerName());
-        return winner;
+        List<Match> matches = matchDAO.getList();
+        return teamDAO.getTeamByName(matches.get(matches.size() - 1).getWinnerName());
     }
 
     public Team getWinner(Match match) {
-        Team winner = TeamDAO.getTeamByName(match.getWinnerName());
-        return winner;
+        return teamDAO.getTeamByName(match.getWinnerName());
     }
+
+    public void saveAllTeams(List<Team> teamList) {
+        teamDAO.save(teamList);
+    }
+
+    public List<Team> getAllTeams() {
+        return teamDAO.getList();
+    }
+
+    public List<Match> getAllMatches() {
+        return matchDAO.getList();
+    }
+
 }
