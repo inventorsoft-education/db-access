ec74de9fec0646ed02fb72c4e877eb9294dc6739
Dmytro Svirniuk <dmytro123svi@gmail.com>
2022-10-03 22:22:40 +0300

    fixed comments

--- src/main/java/com/example/springresttask/repository/EmailRepositoryImpl.java
+++ src/main/java/com/example/springresttask/repository/EmailRepositoryImpl.java
@@ -40,33 +40,33 @@ public class EmailRepositoryImpl implements EmailRepository {
     public List<Email> findAllByPendingEmail() {
 
         List<Email> emails = new ArrayList<>();
-        try {
+        try (Connection connection = getConnection()) {
             String sql = """
                         SELECT id,recipient_name,email_subject,email_body,delivery_date, is_sent
                         FROM email
                         WHERE is_sent IS FALSE;
                     """;
-            Connection connection = getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            resultSet = preparedStatement.executeQuery();
 
-            while (resultSet.next()) {
-                Email email = new Email();
-                email.setId(resultSet.getLong("id"));
-                email.setRecipientName(resultSet.getString("recipient_name"));
-                email.setEmailSubject(resultSet.getString("email_subject"));
-                email.setEmailBody(resultSet.getString("email_body"));
-                email.setDeliveryDate(resultSet.getTimestamp(5).toLocalDateTime());
-                email.setSent(resultSet.getBoolean("is_sent"));
-                emails.add(email);
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+                resultSet = preparedStatement.executeQuery();
+
+                while (resultSet.next()) {
+                    Email email = new Email();
+                    email.setId(resultSet.getLong("id"));
+                    email.setRecipientName(resultSet.getString("recipient_name"));
+                    email.setEmailSubject(resultSet.getString("email_subject"));
+                    email.setEmailBody(resultSet.getString("email_body"));
+                    email.setDeliveryDate(resultSet.getTimestamp(5).toLocalDateTime());
+                    email.setSent(resultSet.getBoolean("is_sent"));
+                    emails.add(email);
+                }
             }
-            connection.close();
-            preparedStatement.close();
+
             resultSet.close();
         } catch (SQLException e) {
             log.info("there are no such letters");
         }
-
 
         return emails;
     }
@@ -80,12 +80,10 @@ public class EmailRepositoryImpl implements EmailRepository {
                     WHERE is_sent IS FALSE
                     AND  e.id=  ?;
                 """;
-        try {
-            Connection connection = getConnection();
+        try (Connection connection = getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
-            connection.close();
             preparedStatement.close();
         } catch (SQLException ex) {
             log.info("Deleting  is failed");
@@ -99,17 +97,16 @@ public class EmailRepositoryImpl implements EmailRepository {
                     INSERT INTO email (recipient_name,email_subject,email_body,delivery_date,is_sent)
                     VALUES (?,?,?,?,?);
                 """;
-        try {
-            Connection connection = getConnection();
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setString(1, email.getRecipientName());
-            ps.setString(2, email.getEmailSubject());
-            ps.setString(3, email.getEmailBody());
-            ps.setTimestamp(4, Timestamp.valueOf(email.getDeliveryDate()));
-            ps.setBoolean(5, email.isSent());
-            ps.executeUpdate();
-            connection.close();
-            ps.close();
+        try (Connection connection = getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+
+                ps.setString(1, email.getRecipientName());
+                ps.setString(2, email.getEmailSubject());
+                ps.setString(3, email.getEmailBody());
+                ps.setTimestamp(4, Timestamp.valueOf(email.getDeliveryDate()));
+                ps.setBoolean(5, email.isSent());
+                ps.executeUpdate();
+            }
         } catch (SQLException e) {
             log.info("You cannot save this email");
             e.printStackTrace();
@@ -123,20 +120,20 @@ public class EmailRepositoryImpl implements EmailRepository {
                 UPDATE email set recipient_name = ?,email_subject = ?,email_body =? , delivery_date = ?, is_sent =  ?
                 WHERE id =?;
                 """;
-        try {
-            Connection connection = getConnection();
-            PreparedStatement ps =
-                    connection.prepareStatement(sql);
+        try (Connection connection = getConnection()) {
 
-            ps.setString(1, email.getRecipientName());
-            ps.setString(2, email.getEmailSubject());
-            ps.setString(3, email.getEmailBody());
-            ps.setTimestamp(4, Timestamp.valueOf(email.getDeliveryDate()));
-            ps.setBoolean(5, email.isSent());
-            ps.setLong(6, email.getId());
-            ps.executeUpdate();
-            connection.close();
-            ps.close();
+            try (PreparedStatement ps =
+                         connection.prepareStatement(sql)) {
+
+
+                ps.setString(1, email.getRecipientName());
+                ps.setString(2, email.getEmailSubject());
+                ps.setString(3, email.getEmailBody());
+                ps.setTimestamp(4, Timestamp.valueOf(email.getDeliveryDate()));
+                ps.setBoolean(5, email.isSent());
+                ps.setLong(6, email.getId());
+                ps.executeUpdate();
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
