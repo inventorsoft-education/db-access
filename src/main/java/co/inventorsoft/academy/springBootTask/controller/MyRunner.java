849bfdb60b5eb447d8b3dfb087c001549a12b1f1
Bohdan Shostak <bohdanshostak93@gmail.com>
2022-10-13 17:34:33 +0300

    correct CommandLineRunner

--- src/main/java/co/inventorsoft/academy/springBootTask/controller/MyRunner.java
+++ src/main/java/co/inventorsoft/academy/springBootTask/controller/MyRunner.java
@@ -3,7 +3,6 @@ package co.inventorsoft.academy.springBootTask.controller;
 import co.inventorsoft.academy.springBootTask.service.TeamService;
 import co.inventorsoft.academy.springBootTask.service.TournamentBracketService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -16,8 +15,7 @@ public class MyRunner implements CommandLineRunner {
     private final TeamService teamService;
     private final TournamentBracketService tbService;
 
-    @Autowired
-    private ConfigurableApplicationContext context;
+    private final ConfigurableApplicationContext context;
 
     @Override
     public void run(String... args) {
