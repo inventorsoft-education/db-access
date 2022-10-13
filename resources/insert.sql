b59f46cf332cfd3ad0f93a99f34e7fd815284fc0
ann <a.savuliak@gmail.com>
2022-10-13 18:21:16 +0300

    delete unused files and classes

--- resources/insert.sql
+++
@@ -1,8 +0,0 @@
-insert into teams (id, team_name, horse, rider) values (null, 'team1', 'horse1', 'rider1');
-insert into teams (id, team_name, horse, rider) values (null, 'team2', 'horse2', 'rider2');
-insert into teams (id, team_name, horse, rider) values (null, 'team3', 'horse3', 'rider3');
-insert into teams (id, team_name, horse, rider) values (null, 'team4', 'horse4', 'rider4');
-insert into teams (id, team_name, horse, rider) values (null, 'team5', 'horse5', 'rider5');
-insert into teams (id, team_name, horse, rider) values (null, 'team6', 'horse6', 'rider6');
-insert into teams (id, team_name, horse, rider) values (null, 'team7', 'horse7', 'rider7');
-insert into teams (id, team_name, horse, rider) values (null, 'team8', 'horse8', 'rider8');
