fd4592fde1e08d2adc309754851d1ead0d29efa7
Bohdan Shostak <bohdanshostak93@gmail.com>
2022-10-14 12:37:44 +0300

    Remove annotation from Repository interfaces

--- src/main/java/co/inventorsoft/academy/springBootTask/repository/TeamRepository.java
+++ src/main/java/co/inventorsoft/academy/springBootTask/repository/TeamRepository.java
@@ -1,11 +1,9 @@
 package co.inventorsoft.academy.springBootTask.repository;
 
 import co.inventorsoft.academy.springBootTask.domain.entity.Team;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface TeamRepository {
 
     List<Team> findAll();
--- src/main/java/co/inventorsoft/academy/springBootTask/repository/TournamentBracketRepository.java
+++ src/main/java/co/inventorsoft/academy/springBootTask/repository/TournamentBracketRepository.java
@@ -1,11 +1,9 @@
 package co.inventorsoft.academy.springBootTask.repository;
 
 import co.inventorsoft.academy.springBootTask.domain.entity.TournamentBracket;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface TournamentBracketRepository {
 
     TournamentBracket save(TournamentBracket tb);
